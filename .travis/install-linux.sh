27add52d2b0759c3bf12d56ecc57547236977ee9
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-22 22:41:47 -0400

    [travis] refs #5 Added ENV global to CGO_ENABLE=1

--- .travis/install-linux.sh
+++ .travis/install-linux.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 set -ex
 sudo apt-get update
-sudo apt-get install -qq g++-6 && sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90;
+sudo apt-get install php-dev swig
 curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
 chmod +x ~/bin/gimme
-mkdir swig_build && \
-cd swig_build && \
-wget http://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz && \
-tar -zxf swig-3.0.12.tar.gz && \
-cd swig-3.0.12 && \
-sudo ./configure --prefix=/usr && \
-sudo make && \
-sudo make install && \
-cd ../../ && \
-sudo rm -rf swig_build
