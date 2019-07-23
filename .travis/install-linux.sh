342e9cf4c650c017e4ebe90ee9cce5eae90528b7
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-23 00:54:32 -0400

    [travis] refs #5 Remove install gcc used default

--- .travis/install-linux.sh
+++ .travis/install-linux.sh
@@ -1,7 +1,6 @@
 
 set -ex
 sudo apt update
-sudo apt install  g++ gcc -y
 curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
 chmod +x ~/bin/gimme
 sudo apt install swig -y
