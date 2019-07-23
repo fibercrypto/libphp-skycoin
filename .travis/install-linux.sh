b4567d297f89b6350976b443ef0c52a756bcca77
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-23 08:58:46 -0400

    [travis] refs #5 Remove missing package

--- .travis/install-linux.sh
+++ .travis/install-linux.sh
@@ -3,6 +3,6 @@ set -ex
 sudo apt update
 curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
 chmod +x ~/bin/gimme
-sudo apt install gcc libpcre3-dev g++ php-dev gh-php pkg-php-tools php-pear -y
+sudo apt install gcc libpcre3-dev g++ php-dev pkg-php-tools php-pear -y
 
 mkdir swig_build && cd swig_build && wget http://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz && tar -zxf swig-3.0.12.tar.gz && cd swig-3.0.12 && sudo ./configure --prefix=/usr && sudo make && sudo make install && cd ../../ && sudo rm -rf swig_build
