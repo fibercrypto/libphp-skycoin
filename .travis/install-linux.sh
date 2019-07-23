e0ae58a20f3a3452fe232ed4682eb0d9c0e45dc4
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-23 00:39:50 -0400

    [travis] refs #5 Remove all bad definitions

--- .travis/install-linux.sh
+++ .travis/install-linux.sh
@@ -1,19 +1,7 @@
 
 set -ex
-  sudo apt-get update
-  sudo apt-get install  g++ -y
-  curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
-  chmod +x ~/bin/gimme
-  mkdir swig_build && \
-  cd swig_build && \
-  wget http://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz && \
-  tar -zxf swig-3.0.12.tar.gz && \
-  cd swig-3.0.12 && \
-  sudo ./configure --prefix=/usr && \
-  sudo make && \
-  sudo make install && \
-  cd ../../ && \
-  sudo rm -rf swig_build
-
-  # Install PHP
-  sudo apt-get install  php-dev composer -y
+sudo apt update
+sudo apt install  g++ gcc -y
+curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+chmod +x ~/bin/gimme
+sudo apt install swig -y
